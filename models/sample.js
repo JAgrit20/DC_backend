a782db57af81ffcbb45dc3fcb8bfbc43f4b2e2f7
JAgrit20 <jagritacharya2020@gmail.com>
2021-12-15 11:37:36 +0530

    student schema added

--- models/sample.js
+++ models/sample.js
@@ -1,14 +1,56 @@
 import mongoose from 'mongoose'
 
-const sampleSchema = new mongoose.Schema({
-  field1: {
+const Student = new mongoose.Schema({
+  Board : {
     type: String,
   },
-  field2: {
+  City : {
+    type: String,
+  },
+  Email  : {
+    type: String,
+  },
+  Grade  : {
+    type: String,
+  },
+  Institute  : {
+    type: String,
+  },
+  State  : {
+    type: String,
+  },
+  dob  : {
+    type: String,
+  },
+  firstname  : {
+    type: String,
+  },
+  City : {
+    type: String,
+  },
+  lastname  : {
+    type: String,
+  },
+  pdoubts  : {
+    type: Integer,
+  },
+  phonenumber  : {
+    type: Integer,
+  },
+  photoUrl  : {
+    type: String,
+  },
+  slang   : {
+    type: String,
+  },
+  tdoubts   : {
+    type: Integer,
+  },
+  validity   : {
     type: String,
   },
 })
 
-const Sample = mongoose.model('Sample', sampleSchema)
+const Sample = mongoose.model('Sample', Student)
 
 export default Sample
